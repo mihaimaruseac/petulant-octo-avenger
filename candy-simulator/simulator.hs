e1cdead24d9cfbe11ee6dfafd63f5b67b8364e24
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-02-28 18:29:54 -0500

    Simulate one serving.

--- candy-simulator/simulator.hs
+++ candy-simulator/simulator.hs
@@ -4,3 +4,18 @@
  - game of Pardus (http://pardus.at), based on the amount of sugar already in
  - his blood and the value of trip-control/meditation skill (TC).
  -}
+
+import System.Random.TF
+import System.Random.TF.Gen
+import System.Random.TF.Instances
+
+type TripControl = Int
+type SugarInSystem = Int
+type Candy = Int
+type AP = Int
+
+generateAPs :: RandomGen g => TripControl -> SugarInSystem -> Candy -> g -> (AP, g)
+generateAPs tc sg cd g = (appt * cd - 8 * s, g')
+  where
+    (appt, g') = randomR (200, 250) g
+    s = sum [sg .. sg + cd - 1]

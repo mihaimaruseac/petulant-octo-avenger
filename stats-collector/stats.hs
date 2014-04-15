2c2967b9f4d5dd203103a165ce10d62258da7938
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-04-15 14:00:00 -0400

    Use the proper filter.

--- stats-collector/stats.hs
+++ stats-collector/stats.hs
@@ -10,7 +10,7 @@ main = do
   print devs
   putStrLn "Opening capture device"
   h <- openLive "any" gSnapshotSize False 0
-  setFilter h "tcp port 8000" True 0
+  setFilter h "host artemis.pardus.at" True 0
   pRead <- loopBS h (- 1) cb
   putStrLn $ "Read " ++ show pRead ++ " packets"
 

1fff2efc74f44c97a7259282de9e6d20ec9cbc8a
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-04-19 14:00:00 -0400

    Handle universe type and edit callback

--- stats-collector/stats.hs
+++ stats-collector/stats.hs
@@ -4,20 +4,30 @@
  - Extract statistics about the Artemis universe of Pardus.
  -}
 import Network.Pcap
+import System.Environment
 
 gSnapshotSize = 1000000000
 
 main = do
-  putStrLn "Opening capture device"
-  h <- openLive "any" gSnapshotSize False 0
-  setFilter h "host artemis.pardus.at" True 0
-  pRead <- loopBS h (- 1) cb
+  args <- getArgs
+  case args of
+    [] -> statsOn "artemis"
+    u:_ -> statsOn u
+
+statsOn universe = do
+  handle <- openLive "any" gSnapshotSize False 0
+  setFilter handle (buildFilter universe) True 0
+  pRead <- loopBS handle (- 1) mainCallback
   putStrLn $ "Read " ++ show pRead ++ " packets"
 
-cb :: CallbackBS
-cb h bs
-  | hdrWireLength h > hdrCaptureLength h = putStrLn $ "Incomplete packet captured" ++ show h
-  | otherwise = do
-    print h
-    --print bs
-    return ()
+buildFilter universe = concat ["host ", universe, ".pardus.at"]
+
+mainCallback header payload
+  | hdrWireLength header > hdrCaptureLength header = incomplete header
+  | otherwise = process header payload
+
+incomplete header = putStrLn $ "Incomplete packet captured" ++ show header
+
+process header payload = do
+    print header
+    --print payload

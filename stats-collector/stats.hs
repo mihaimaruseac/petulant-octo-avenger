8aae774e6edcad21d563285782aa7d1d49f5f037
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-04-26 11:02:35 -0400

    Display hex of payload and get link layer length.

--- stats-collector/stats.hs
+++ stats-collector/stats.hs
@@ -2,13 +2,18 @@
 {-# LANGUAGE RecordWildCards #-}
 
 import Network.Pcap
+import Numeric
 import System.Environment
+
+import qualified Data.ByteString as B
 
 gSnapshotSize :: Int
 gSnapshotSize = 1000000000
 
 gDefaultUniverse :: String
 gDefaultUniverse = "artemis"
+
+type LinkLength = Integer
 
 main :: IO ()
 main = do
@@ -19,25 +24,32 @@ main = do
 
 statsOn :: String -> IO ()
 statsOn universe = do
-  putStrLn $ concat ["Capturing on ", universe]
-  putStrLn "Press ^C twice to end"
   handle <- openLive "any" gSnapshotSize False 0
   setFilter handle (buildFilter universe) True 0
-  pRead <- loopBS handle (- 1) mainCallback
+  link <- datalink handle
+  let hdrLen = linkHdrLen link
+  putStrLn $ concat ["Capturing on ", universe]
+  putStrLn "Press ^C twice to end"
+  pRead <- loopBS handle (- 1) $ mainCallback hdrLen
   putStrLn $ "Read " ++ show pRead ++ " packets"
 
 buildFilter :: String -> String
 buildFilter universe = concat ["host ", universe, ".pardus.at"]
 
-mainCallback :: CallbackBS
-mainCallback h@PktHdr{..}
+linkHdrLen :: Link -> LinkLength
+linkHdrLen DLT_LINUX_SLL = 12
+linkHdrLen l = error $ concat ["Unknown link header ", show l]
+
+mainCallback :: LinkLength -> CallbackBS
+mainCallback hdrLen h@PktHdr{..}
   | hdrWireLength > hdrCaptureLength = incomplete h
-  | otherwise = process h
+  | otherwise = process hdrLen h
 
 incomplete :: CallbackBS
 incomplete header _ = putStrLn $ "Incomplete packet captured" ++ show header
 
-process :: CallbackBS
-process header payload = do
+process :: LinkLength -> CallbackBS
+process hdrLen header payload = do
     print header
     --print payload
+    putStrLn . concatMap (flip showHex "") . B.unpack $ payload

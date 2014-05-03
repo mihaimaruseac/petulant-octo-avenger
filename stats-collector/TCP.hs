3c1d92d74bd8b5698f96750d8c4cb4e772c37419
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-03 14:00:00 -0400

    Parse TCP flags.

--- stats-collector/TCP.hs
+++ stats-collector/TCP.hs
@@ -13,7 +13,18 @@ data TCP = TCP
   , tcpDPort :: Word16
   , tcpSeqNr :: Word32
   , tcpAckNr :: Word32
+  , tcpDtOff :: Int
+  , tcpFlags :: [TCPFlags]
   } deriving Show
+
+data TCPFlags
+  = TCPURG
+  | TCPACK
+  | TCPPSH
+  | TCPRST
+  | TCPSYN
+  | TCPFIN
+  deriving (Show, Eq, Enum)
 
 parseTCP :: Get TCP
 parseTCP = do
@@ -21,4 +32,13 @@ parseTCP = do
   dp <- getWord16be
   sq <- getWord32be
   ack <- getWord32be
-  return $ TCP sp dp sq ack
+  fo <- getWord16be
+  when (fo .&. 0x0fc0 /= 0) $ error "Invalid TCP packet: invalid reserved bits"
+  let offset = fromIntegral $ 4 * ((fo .&. 0xf000) `shiftR` 12)
+  let flags = getFlags (fo .&. 0x03f)
+  return $ TCP sp dp sq ack offset flags
+
+getFlags :: Word16 -> [TCPFlags]
+getFlags f = map fst . filter ((/= 0) . snd) . zip [TCPURG .. TCPFIN] . map ($ f) $ fields
+  where
+    fields = [(.&. 0x20), (.&. 0x10), (.&. 0x08), (.&. 0x04), (.&. 0x02), (.&. 0x01)]

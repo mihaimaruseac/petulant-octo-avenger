bece7e0d27093e0aa68a715cdb957cb8ffb9899f
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-12 12:34:15 -0400

    Enumerator/Iteratee seems to work perfectly good.

--- stats-collector/Types.hs
+++ stats-collector/Types.hs
@@ -1,7 +1,10 @@
 module Types where
 
+import Network.Pcap
+
 import qualified Data.ByteString as B
 
 type LinkLength = Int
 type Payload = B.ByteString
+type CookedPacket = (PktHdr, B.ByteString)
 type ProcessPacket = Payload -> IO ()
--- stats-collector/stats.hs
+++ stats-collector/stats.hs
@@ -17,6 +17,7 @@ import Data.Char
 import Control.Applicative
 import Control.Exception
 import Data.Enumerator hiding (map, filter)
+import qualified Data.Enumerator.List as DEL
 
 main :: IO ()
 main = do
@@ -33,7 +34,7 @@ statsOn universe = do
   let hdrLen = linkHdrLen link
   putStrLn $ concat ["Capturing on ", universe]
   putStrLn "Press ^C to end"
-  run_ $ iterateeChain handle
+  run_ $ iterateeChain handle hdrLen
 
 buildFilter :: String -> String
 buildFilter universe = concat ["host ", universe, ".pardus.at"]
@@ -42,14 +43,16 @@ linkHdrLen :: Link -> LinkLength
 linkHdrLen DLT_LINUX_SLL = 16 -- TODO we should check that IP is next layer
 linkHdrLen l = error $ concat ["Unknown link header ", show l]
 
---iterateeChain :: PcapHandle -> Iteratee (PktHdr, B.ByteString) m ()
-iterateeChain h =
+--iterateeChain :: PcapHandle -> Iteratee CookedPacket m ()
+iterateeChain h hdrLen =
   -- enumerate all packets
   packetEnumerator h $$
+  -- drop cooked frame
+  dropCookedFrame hdrLen =$
   -- print everything of value (debugging)
   printChunks False
 
-packetEnumerator :: MonadIO m => PcapHandle -> Enumerator (PktHdr, B.ByteString) m b
+packetEnumerator :: MonadIO m => PcapHandle -> Enumerator CookedPacket m b
 packetEnumerator h = list
   where
     list (Continue k) = do
@@ -57,19 +60,18 @@ packetEnumerator h = list
       k (Chunks $ if hdrCaptureLength hdr == 0 then [] else [pkt]) >>== list
     list step = returnI step
 
-{-
-  pRead <- loopBS handle (- 1) $ mainCallback hdrLen
-  putStrLn $ concat ["Read ", show pRead, " packets"]
-  -}
+dropCookedFrame :: Monad m => LinkLength -> Enumeratee CookedPacket Payload m b
+dropCookedFrame hdrLen = DEL.map f
+  where
+    f (h@PktHdr{..}, payload)
+      | hdrWireLength > hdrCaptureLength = error "Incomplete capture"
+      | otherwise = B.drop hdrLen payload
 
 {-
 mainCallback :: LinkLength -> CallbackBS
 mainCallback hdrLen h@PktHdr{..} payload
   | hdrWireLength > hdrCaptureLength = incomplete h payload
   | otherwise = processIP $ B.drop hdrLen payload
-
-incomplete :: CallbackBS
-incomplete header _ = putStrLn $ concat ["Incomplete packet captured ", show header]
 
 processIP :: ProcessPacket
 processIP payload = case runGetPartial parseIP payload of

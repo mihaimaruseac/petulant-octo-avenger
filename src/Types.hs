71e6af1d71f8d93e54e679d260ddb7d5e5074cad
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-09-03 09:13:00 -0400

    Reorder types for better readability.

--- src/Types.hs
+++ src/Types.hs
@@ -9,33 +9,27 @@ import qualified Data.ByteString as B
 
 import TCP
 
-type LinkLength = Int
-type Payload = B.ByteString
-
+type ChanneledHeaderRequest = (HTTPRequestType, URI, [RequestHeader], RequestPayload, [ResponseHeader], ResponsePayload)
+type ChanneledRequest = (HTTPRequestType, URI, RequestPayload, RequestPayload, ResponsePayload, ResponsePayload)
 type CookedPacket = (PktHdr, Payload)
-type TCPConversation = [TCPPayload]
-type Port = Word16
-type SeqNo = Word32
-
-type HeaderType = Payload
-type HeaderValue = Payload
-type Header = (HeaderType, HeaderValue)
-
-type RequestPayload = Payload
-type RequestHeader = Header
-type ResponsePayload = Payload
-type ResponseHeader = Header
-type URI = Payload
-
-type TCPPayload = (TCP, Payload)
-type Request = (RequestPayload, ResponsePayload)
 type HTTPTaggedRequest = (HTTPRequestType, RequestPayload, ResponsePayload)
 type HTTPURIRequest = (HTTPRequestType, URI, RequestPayload, ResponsePayload)
-type ChanneledRequest = (HTTPRequestType, URI, RequestPayload, RequestPayload, ResponsePayload, ResponsePayload)
-type ChanneledHeaderRequest = (HTTPRequestType, URI, [RequestHeader], RequestPayload, [ResponseHeader], ResponsePayload)
+type Header = (HeaderType, HeaderValue)
+type HeaderType = Payload
+type HeaderValue = Payload
+type LinkLength = Int
+type Payload = B.ByteString
+type Port = Word16
+type Request = (RequestPayload, ResponsePayload)
+type RequestHeader = Header
+type RequestPayload = Payload
+type ResponseHeader = Header
+type ResponsePayload = Payload
+type SeqNo = Word32
 type TCPC = (TCPConversationState, TCPConversation)
-
-
+type TCPConversation = [TCPPayload]
+type TCPPayload = (TCP, Payload)
+type URI = Payload
 
 data TCPConversationState = Ongoing | CloseFin | CloseFinACK | CloseACK
   deriving (Eq, Show, Ord, Enum)

3501d6c8aaca32e79163ea123c7a760322fa75f8
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-01-07 17:29:00 -0500

    Final setup before needing to parse the HTML and map to request types.

--- src/Tag.hs
+++ src/Tag.hs
@@ -1,12 +1,20 @@
 module Tag (tagAndStore) where
 
+import qualified Data.ByteString as B
+
 import Types
+
+data DBCommand
+  = Game Int
+  deriving Show
 
 data TaggedInfo
   = Fail ChanneledHeaderRequest
+  | OK DBCommand
 
 instance Show TaggedInfo where
   show (Fail chr) = '#' : ' ' : show chr
+  show (OK dbc) = show dbc
 
 tagAndStore :: ChanneledHeaderRequest -> TaggedInfo
-tagAndStore = Fail
+tagAndStore e@(rt, uri, rqhs, rqp, rphs, rpp) = OK $ Game $ B.length rpp

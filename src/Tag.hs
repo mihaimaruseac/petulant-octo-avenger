907c07d90692a872765c737ba67b5ce7be8af9d3
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-02-08 12:58:00 -0500

    Differentiate between general StatsSM and StatsSM on [Tag Payload] (StatsPSM).

--- src/Tag.hs
+++ src/Tag.hs
@@ -11,13 +11,14 @@ import qualified Data.ByteString.Char8 as C
 import Errors
 import Types
 
-type StatsSM a = StateT [Tag Payload] StatsM a
+type StatsSM s a = StateT s StatsM a
+type StatsPSM a = StatsSM [Tag Payload] a
 
-runStatsSM :: [Tag Payload] -> StatsSM a -> StatsM (a, [Tag Payload])
-runStatsSM tags m = runStateT m tags
+runStatsSM :: s -> StatsSM s a -> StatsM (a, s)
+runStatsSM s m = runStateT m s
 
-evalStatsSM :: [Tag Payload] -> StatsSM a -> StatsM a
-evalStatsSM tags m = case runStatsSM tags m of
+evalStatsSM :: s -> StatsSM s a -> StatsM a
+evalStatsSM s m = case runStatsSM s m of
   Right (a, _) -> return a
   Left e -> throwError e
 
@@ -76,24 +77,24 @@ extractAttrib attrib tag
   where
     t = fromAttrib attrib tag
 
-obtainFieldInfo :: [Tag Payload] -> (Tag Payload -> StatsM a) -> StatsSM a
+obtainFieldInfo :: [Tag Payload] -> (Tag Payload -> StatsM a) -> StatsPSM a
 obtainFieldInfo tgs f = searchByTags tgs >>= (lift . f)
 
-obtainFieldInfoN :: [(Int, Tag Payload)] -> (Tag Payload -> StatsM a) -> StatsSM a
+obtainFieldInfoN :: [(Int, Tag Payload)] -> (Tag Payload -> StatsM a) -> StatsPSM a
 obtainFieldInfoN tgs f = searchByTagsN tgs >>= (lift . f)
 
-searchByTags :: [Tag Payload] -> StatsSM (Tag Payload)
+searchByTags :: [Tag Payload] -> StatsPSM (Tag Payload)
 searchByTags [] = throwError $ CodingError "Should always have at least on tag to search for"
 searchByTags tgs = foldM (flip $ const . findTag) undefined tgs
 
-searchByTagsN :: [(Int, Tag Payload)] -> StatsSM (Tag Payload)
+searchByTagsN :: [(Int, Tag Payload)] -> StatsPSM (Tag Payload)
 searchByTagsN [] = throwError $ CodingError "Should always have at least on tag to search for"
 searchByTagsN tgs = foldM (flip $ const . uncurry findTagN) undefined tgs
 
-findTag :: Tag Payload -> StatsSM (Tag Payload)
+findTag :: Tag Payload -> StatsPSM (Tag Payload)
 findTag = findTagN 1
 
-findTagN :: Int -> Tag Payload -> StatsSM (Tag Payload)
+findTagN :: Int -> Tag Payload -> StatsPSM (Tag Payload)
 findTagN n t
   | n <= 0 = throwError $ CodingError "Should never require non-positive tags"
   | otherwise = do

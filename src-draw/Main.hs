44013fee25ce99260e53c8e1a8a4329fd416e941
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-07 17:24:00 -0500

    Have first version of demo seemingly work.

--- src-draw/Main.hs
+++ src-draw/Main.hs
@@ -1,9 +1,15 @@
 -- {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Prelude
 import System.Environment
 
 main :: IO ()
 main = do
   args <- getArgs
-  print args
+  case args of
+    ["1"] -> firstDemo
+    _ -> print args
+
+firstDemo = undefined

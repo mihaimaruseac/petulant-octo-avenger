3a57b60231974870a696c9c3befa997936bf1962
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-11 21:56:00 -0400

    Attempt at having cmdline arg to select image to draw.

--- src-draw/Main.hs
+++ src-draw/Main.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 import Diagrams.Backend.Rasterific.CmdLine
 import Diagrams.Prelude
 import System.Environment
 
 main :: IO ()
-main = firstDemo
-{-do
+main = do
   args <- getArgs
   case args of
-    "1":_ -> firstDemo
+    ["1"] -> mainWith firstDemo
     _ -> print args
-    -}
+    --}
+--firstDemo
 
-firstDemo = mainWith (circle 1 :: Diagram B R2)
+firstDemo :: Diagram B R2
+firstDemo = circle 1 -- :: Diagram B R2)

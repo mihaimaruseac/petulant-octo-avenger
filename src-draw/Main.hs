1fc32c97d091de9c2485ca7853a2cf27b15dd738
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-12 22:12:00 -0400

    Mish-mash of code.

--- src-draw/Main.hs
+++ src-draw/Main.hs
@@ -1,17 +1,49 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
+import Diagrams.Backend.CmdLine
 import Diagrams.Backend.Rasterific.CmdLine
-import Diagrams.Prelude
 import System.Environment
 
+import Diagrams.Prelude hiding ((<>))
+import Options.Applicative
+
+{-
+data Selection
+  = FirstDemo
+  deriving (Eq, Show)
+
+data FlipOpts = FlipOpts Bool
+
+instance Parseable FlipOpts where
+  parser = FlipOpts <$> switch (long "flipped" <> help "Flip the diagram L-R")
+
 main :: IO ()
-main = do
+main = mainWith (\(FlipOpts f) -> (if f then reflectX else id) d)
+{-do
   args <- getArgs
+  mainWith $ genSelection args
+  -}
+
+genSelection :: a -> Selection
+genSelection = undefined
+
+{-
   case args of
     ["1"] -> mainWith firstDemo
     _ -> print args
     --}
 --firstDemo
 
+{-
+switch :: Selection -> Diagram B R2
+switch FirstDemo = firstDemo
+-}
+
 firstDemo :: Diagram B R2
 firstDemo = circle 1 -- :: Diagram B R2)
+
+d = firstDemo
+-}
+
+main :: IO ()
+main = mainWith (circle 1 :: Diagram B R2)

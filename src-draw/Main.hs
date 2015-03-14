ed4178a4d831849adb86ac652924489af42e44f4
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-13 23:51:50 -0400

    More demos and comments

--- src-draw/Main.hs
+++ src-draw/Main.hs
@@ -19,9 +19,21 @@ demos =
   , demoCircleStyled
   , demoTwoCircles
   , demoAtop
+  , demoOrigin
+  , demoOrigin2
+  , demoSide
+  , demoSideOrigin
   ]
   where
     demoCircle = circle 1
     demoCircleStyled = circle 1 # fc blue # lw veryThick # lc purple # dashingG [0.2, 0.05] 0
-    demoTwoCircles = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none
     demoAtop = square 1 # fc aqua `atop` circle 1
+    -- show origins
+    demoOrigin = circle 1 # showOrigin
+    demoOrigin2 = demoAtop # showOrigin
+    -- stack horizontally
+    demoTwoCircles = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none
+    -- stack vertically
+    demoSide = circle 1 === square 2
+    -- origin is origin of first element
+    demoSideOrigin = demoSide # showOrigin

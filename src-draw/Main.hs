842a560e75977d52da82d831c289a2f308044387
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-13 23:42:40 -0400

    Cleanup

--- src-draw/Main.hs
+++ src-draw/Main.hs
@@ -1,46 +1,7 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
-import Diagrams.Backend.CmdLine
 import Diagrams.Backend.Rasterific.CmdLine
-import System.Environment
-
-import Diagrams.Prelude hiding ((<>))
-import Options.Applicative
-
-{-
-data Selection
-  = FirstDemo
-  deriving (Eq, Show)
-
-data FlipOpts = FlipOpts Bool
-
-instance Parseable FlipOpts where
-  parser = FlipOpts <$> switch (long "flipped" <> help "Flip the diagram L-R")
-
-main :: IO ()
-main = mainWith (\(FlipOpts f) -> (if f then reflectX else id) d)
-{-do
-  args <- getArgs
-  mainWith $ genSelection args
-  -}
-
-genSelection :: a -> Selection
-genSelection = undefined
-
-{-
-  case args of
-    ["1"] -> mainWith firstDemo
-    _ -> print args
-    --}
---firstDemo
-
-{-
-switch :: Selection -> Diagram B R2
-switch FirstDemo = firstDemo
--}
-d = firstDemo
--}
-
+import Diagrams.Prelude
 
 type Demo = Int
 

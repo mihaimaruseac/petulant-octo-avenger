2f60f918107f4d1226cff389d3e7cbc1f9836023
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-20 22:08:00 -0400

    Reformat Demos.hs module

--- src-draw/Demos.hs
+++ src-draw/Demos.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
-
 module Demos (Demo(..), selectDemo, demoTournament) where
 
 import Diagrams.Backend.Rasterific.CmdLine
@@ -13,6 +11,9 @@ selectDemo :: Demo -> Diagram B R2
 selectDemo (Tutorial n) = doSelectDemo demos n
 selectDemo (Vector n) = doSelectDemo vectorDemos n
 
+demoTournament :: Diagram B R2
+demoTournament = tournament 16
+
 doSelectDemo :: [Diagram B R2] -> Int -> Diagram B R2
 doSelectDemo ds n
   | and [0 < n, n <= l] = ds !! (n - 1)
@@ -25,10 +26,6 @@ vectorDemos =
   [ demoAtop
   , demoCircleStyled
   ]
-  where
-    demoCircle = circle 1
-    demoCircleStyled = circle 1 # fc blue # lw veryThick # lc purple # dashingG [0.2, 0.05] 0
-    demoAtop = square 1 # fc aqua `atop` circle 1
 
 demos :: [Diagram B R2]
 demos =
@@ -47,38 +44,62 @@ demos =
   , demoRotate
   , demoAlign
   ]
-  where
-    demoCircle = circle 1
-    demoCircleStyled = circle 1 # fc blue # lw veryThick # lc purple # dashingG [0.2, 0.05] 0
-    demoAtop = square 1 # fc aqua `atop` circle 1
-    -- show origins
-    demoOrigin = circle 1 # showOrigin
-    demoOrigin2 = demoAtop # showOrigin
-    -- stack horizontally
-    demoTwoCircles = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none
-    -- stack vertically
-    demoSide = circle 1 === square 2
-    -- origin is origin of first element
-    demoSideOrigin = demoSide # showOrigin
-    -- beside uses vector displacements (r2)
-    demoBeside = beside (r2 (1,1)) (circle 1) (square 2)
-    -- horizontal concat (hcat is concat with |||) and strutX for spaces
-    demoHcat = hcat [hcat [demoBeside, demoBeside], strutX 1, demoBeside ||| demoBeside]
-    -- ellipses. Since positioning is based on a separation line perp to the
-    -- center lines there is a gap in between
-    demoEllipse = let ell = circle 1 # scaleX 0.5 # rotateBy (1/6) in ell ||| ell
-    -- solve the above gap issue
-    demoSnug = let ell = circle 1 # scaleX 0.5 # rotateBy (1/6) in ell # snugR <> ell # snugL
-    -- demo rotate by fraction of circle: rotate by 2pi/3 (120 degrees)
-    demoRotate = square 1 # rotateBy (1/3)
-    -- demo align
-    demoAlign = hrule (2 * sum sizes) === circles # centerX
-      where circles = hcat . map alignB . zipWith scale sizes
-                    $ repeat (circle 1)
-            sizes   = [2,5,4,7,1,3]
 
-demoTournament :: Diagram B R2
-demoTournament = tournament 16
+demoCircle :: Diagram B R2
+demoCircle = circle 1
+
+demoCircleStyled :: Diagram B R2
+demoCircleStyled = circle 1 # fc blue # lw veryThick # lc purple # dashingG [0.2, 0.05] 0
+
+demoAtop :: Diagram B R2
+demoAtop = square 1 # fc aqua `atop` circle 1
+
+-- show origins
+demoOrigin :: Diagram B R2
+demoOrigin = circle 1 # showOrigin
+
+demoOrigin2 :: Diagram B R2
+demoOrigin2 = demoAtop # showOrigin
+
+-- stack horizontally
+demoTwoCircles :: Diagram B R2
+demoTwoCircles = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none
+
+-- stack vertically
+demoSide :: Diagram B R2
+demoSide = circle 1 === square 2
+
+-- origin is origin of first element
+demoSideOrigin :: Diagram B R2
+demoSideOrigin = demoSide # showOrigin
+
+-- beside uses vector displacements (r2)
+demoBeside :: Diagram B R2
+demoBeside = beside (r2 (1,1)) (circle 1) (square 2)
+
+-- horizontal concat (hcat is concat with |||) and strutX for spaces
+demoHcat :: Diagram B R2
+demoHcat = hcat [hcat [demoBeside, demoBeside], strutX 1, demoBeside ||| demoBeside]
+
+-- ellipses. Since positioning is based on a separation line perp to the
+-- center lines there is a gap in between
+demoEllipse :: Diagram B R2
+demoEllipse = let ell = circle 1 # scaleX 0.5 # rotateBy (1/6) in ell ||| ell
+
+-- solve the above gap issue
+demoSnug :: Diagram B R2
+demoSnug = let ell = circle 1 # scaleX 0.5 # rotateBy (1/6) in ell # snugR <> ell # snugL
+
+-- demo rotate by fraction of circle: rotate by 2pi/3 (120 degrees)
+demoRotate :: Diagram B R2
+demoRotate = square 1 # rotateBy (1/3)
+
+-- demo align
+demoAlign :: Diagram B R2
+demoAlign = hrule (2 * sum sizes) === circles # centerX
+  where circles = hcat . map alignB . zipWith scale sizes
+                $ repeat (circle 1)
+        sizes   = [2,5,4,7,1,3]
 
 tournament :: Int -> Diagram B R2
 tournament n = decorateTrail (regPoly n 1) (map (node n) [1..]) #

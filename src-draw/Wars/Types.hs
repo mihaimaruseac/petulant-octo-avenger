496bf01e5c8fc37fe60ee68bf9ed71a6fae036d5
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-04-02 19:55:00 -0400

    Seems to be working. Will see when adding details.

--- src-draw/Wars/Types.hs
+++ src-draw/Wars/Types.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Wars.Types where
 
+import Control.Lens
 import Data.Time
 
 data Event = E
@@ -12,3 +15,5 @@ data EventType
   = Peace
   | War -- TODO: add details here or as an extra field above?
   | AllianceConflict
+
+makeLenses ''Event

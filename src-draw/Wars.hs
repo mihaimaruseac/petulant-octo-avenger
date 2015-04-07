7709a4536453ad898e157fe9a6472966c7a45340
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-04-06 22:54:00 -0400

    Artemis events up to the end of the first war.

--- src-draw/Wars.hs
+++ src-draw/Wars.hs
@@ -10,9 +10,12 @@ wars = print events
 events :: [Event]
 events =
   [ mkPeace artemisOpened vendettaStart
-  , mkWar vendettaStart vendettaEnd (Federation, Empire) Empire undefined
+  , mkWar vendettaStart vendettaEnd (Federation, Empire) Empire vendettaDetails
   ]
   where
-    artemisOpened = fromGregorian 2007 6 10
-    vendettaStart = fromGregorian 2008 8 8
-    vendettaEnd   = fromGregorian 2008 9 17
+    artemisOpened   = fromGregorian 2007 6 10
+    vendettaStart   = fromGregorian 2008 8 8
+    vendettaEnd     = fromGregorian 2008 9 17
+    vendettaFed     = Details 315944 185653 15591 114700 0 243 389
+    vendettaEmp     = Details 230784 180219 13165  37400 0 210 322
+    vendettaDetails = (vendettaFed, vendettaEmp)

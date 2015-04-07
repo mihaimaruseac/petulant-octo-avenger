af0cc4bda2dd79e0b92ca944263e5243da27e244
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-04-06 22:48:00 -0400

    Main skeleton for registering the events.

--- src-draw/Wars.hs
+++ src-draw/Wars.hs
@@ -9,8 +9,10 @@ wars = print events
 
 events :: [Event]
 events =
-  [ E artemisOpened vendettaStart Peace
+  [ mkPeace artemisOpened vendettaStart
+  , War   (Duration vendettaStart $ Just vendettaEnd) undefined undefined
   ]
   where
     artemisOpened = fromGregorian 2007 6 10
     vendettaStart = fromGregorian 2008 8 8
+    vendettaEnd   = fromGregorian 2008 9 17
--- src-draw/Wars/Types.hs
+++ src-draw/Wars/Types.hs
@@ -5,16 +5,48 @@ module Wars.Types where
 import Control.Lens
 import Data.Time
 
-data Event = E
-  { _startDate :: Day
-  , _endDate :: Day
-  , _type :: EventType
-  } deriving (Show)
-
-data EventType
-  = Peace
-  | War -- TODO: add details here or as an extra field above?
-  | AllianceConflict
+data Event
+  = Peace { _d :: Duration }
+  | War { _d :: Duration
+        , _factions :: (Faction, Faction)
+        , _details :: (WarDetails, WarDetails)
+        }
+  | LocalConflict { _d :: Duration}
   deriving (Show)
 
+data Duration = Duration
+  { _startDate :: Day
+  , _endDate :: Maybe Day
+  } deriving (Show)
+
+data WarDetails = Details
+  { _points :: Int
+  , _kills :: Int
+  , _structures :: Int
+  , _mission :: Int
+  , _sector :: Int
+  , _heroes :: Int
+  , _medals :: Int
+  } deriving Show
+
+data Faction
+  = Federation
+  | Empire
+  | Union
+  deriving Show
+
+makeLenses ''Duration
 makeLenses ''Event
+makeLenses ''WarDetails
+
+mkPeace :: Day -> Day -> Event
+mkPeace st en = Peace $ Duration st $ Just en
+
+mkPeace' :: Day -> Event
+mkPeace' st = Peace $ Duration st Nothing
+
+mkWar :: Day -> Day -> (Faction, Faction) -> WarDetails -> Event
+mkWar st en fcts det = War (Duration st $ Just en) fcts det
+
+mkWar' :: Day -> (Faction, Faction) -> WarDetails -> Event
+mkWar' st fcts det = War (Duration st Nothing) fcts det

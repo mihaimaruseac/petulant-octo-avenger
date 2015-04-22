b00f514b0b4cf9e344bd46d9baa459825ac2cd95
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-04-22 18:11:15 -0400

    Revert to textBox instead of fwSingleBox.

--- src-draw/Wars.hs
+++ src-draw/Wars.hs
@@ -13,11 +13,11 @@ wars d = drawEvent d $ events !! 5
 
 drawEvent :: Day -> Event -> Diagram B R2
 drawEvent d e
-  | isPeace e         = fwSingleBox lightgreen peaceText
-  | isLocalConflict e = fwSingleBox lightblue evName
+  | isPeace e         = textBox lightgreen peaceText
+  | isLocalConflict e = textBox lightblue evName
                         ===
-                        fwSingleBox lightblue intervalText
-  | isMajorEvent e    = fwSingleBox red $ mconcat [evName, " ", show $ e ^. startDate]
+                        textBox lightblue intervalText
+  | isMajorEvent e    = textBox red $ mconcat [evName, " ", show $ e ^. startDate]
   | isWar e           = buildWarFrame st en evName (e ^. details)
   | otherwise = error $ "Don't know to draw " ++ show e
   where
@@ -27,12 +27,11 @@ drawEvent d e
     intervalText = mconcat ["From ", show st, " to ", show en]
     evName = maybe "" (' ':) $ e ^. name
     peaceText = mconcat [durText, evName, " peace"]
-    fwSingleBox c t = textBox c c t
 
-textBox :: Colour Double -> Colour Double -> String -> Diagram B R2
-textBox c c' t = text t # fc black <> frameBox
+textBox :: Colour Double -> String -> Diagram B R2
+textBox c t = text t # fc black <> frameBox
   where
-    frameBox = rect 50 4 # bg c # lc c' # alignY (-0.6)
+    frameBox = rect 50 4 # bg c # lc c # alignY (-0.6)
 
 buildWarFrame st en n d = fix $ vcat
   [ t 14 n # translate (r2 (350, -12)) <> rect 800 50 # style # translateX 350

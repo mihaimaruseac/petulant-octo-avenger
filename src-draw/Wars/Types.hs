8bfb14b2734a40d673c0593cd0c2d18b3d95d5bc
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-05-03 23:56:00 -0400

    Create Monoid instance (getting maxes) for war details.

--- src-draw/Wars/Types.hs
+++ src-draw/Wars/Types.hs
@@ -5,6 +5,7 @@ module Wars.Types where
 
 import Control.Lens
 import Data.Default
+import Data.Monoid
 import Data.Time
 
 data Event = Event
@@ -46,6 +47,12 @@ instance Ord Faction where
   Empire <= Union = True
   Union <= Federation = True
   x <= y = x == y
+
+instance Monoid WarDetails where
+  mempty = Details 0 0 0 0 0 0 0
+  -- TODO: really fugly
+  (Details p1 k1 s1 m1 sc1 h1 md1) `mappend` (Details p2 k2 s2 m2 sc2 h2 md2)
+    = Details (max p1 p2) (max k1 k2) (max s1 s2) (max m1 m2) (max sc1 sc2) (max h1 h2) (max md1 md2)
 
 makeLenses ''Event
 makeLenses ''EventDetails

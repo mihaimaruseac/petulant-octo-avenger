140e64a90471c5f2fca78ee4371a86cbb866470b
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-06-16 18:55:26 -0400

    Duplicate build, refactor and rename

--- src-draw/Args.hs
+++ src-draw/Args.hs
@@ -51,17 +51,15 @@ parseArgs = execParser $ info (helper <*> parseModes) $
   buildMod "Draw diagrams"
 
 parseModes :: Parser Commands
-parseModes = build (parseDemo Demo) "Draw tutorial demo diagram" "demo"
-         <|> build (parseDemo ADemo) "Draw arrows demo diagram" "arrows"
-         <|> build (parseDemo TDemo) "Draw trails demo diagram" "trails"
-         <|> build (parseDemo VDemo) "Draw vector demo diagram" "vector"
-         <|> build (parseSingle Arrow) "Draw demo arrow diagram" "arrow"
-         <|> build (parseSingle Tournament) "Draw demo tournament diagram" "tournament"
-         <|> build (parseSingle Wars) "Pardus Wars diagrams" "wars"
-         <|> build parseTSSMess "Pardus TSSMess diagrams" "tssmess"
-         <|> build parseNoDiagram "Don't draw anything" "nodia"
-  where
-    build p d c = subparser (command c (p d) <> metavar c)
+parseModes = buildSP (parseDemo Demo) "Draw tutorial demo diagram" "demo"
+         <|> buildSP (parseDemo ADemo) "Draw arrows demo diagram" "arrows"
+         <|> buildSP (parseDemo TDemo) "Draw trails demo diagram" "trails"
+         <|> buildSP (parseDemo VDemo) "Draw vector demo diagram" "vector"
+         <|> buildSP (parseSingle Arrow) "Draw demo arrow diagram" "arrow"
+         <|> buildSP (parseSingle Tournament) "Draw demo tournament diagram" "tournament"
+         <|> buildSP (parseSingle Wars) "Pardus Wars diagrams" "wars"
+         <|> buildSP parseTSSMess "Pardus TSSMess diagrams" "tssmess"
+         <|> buildSP parseNoDiagram "Don't draw anything" "nodia"
 
 buildMod :: String -> InfoMod a
 buildMod d = mconcat
@@ -86,14 +84,16 @@ parseDemo ct d = flip info (buildMod d) . (helper <*>) $ ct
 parseSingle :: (Parseable b) => (b -> a) -> String -> ParserInfo a
 parseSingle f d = flip info (buildMod d) . (helper <*>) $ f <$> parser
 
+buildSP :: (p -> ParserInfo a) -> p -> String -> Parser a
+buildSP parseFun descr c = subparser (command c (parseFun descr) <> metavar c)
+
 parseNoDiagram :: String -> ParserInfo Commands
 parseNoDiagram d = flip info (buildMod d) . (helper <*>) $ pure NoDiagram
 
 parseTSSMess :: String -> ParserInfo Commands
 parseTSSMess d = flip info (buildMod d) . (helper <*>) $ TSSMess <$> subparse
   where
-    subparse = build (parseSingle GameInfo) "Game info" "g"
-           <|> build (parseSingle RoleInfo) "Role info" "r"
-           <|> build (parseSingle PlayerInfo) "Player info" "p"
-           <|> build (parseSingle MechInfo) "Special mechanics" "m"
-    build p d c = subparser (command c (p d) <> metavar c)
+    subparse = buildSP (parseSingle GameInfo) "Game info" "g"
+           <|> buildSP (parseSingle RoleInfo) "Role info" "r"
+           <|> buildSP (parseSingle PlayerInfo) "Player info" "p"
+           <|> buildSP (parseSingle MechInfo) "Special mechanics" "m"

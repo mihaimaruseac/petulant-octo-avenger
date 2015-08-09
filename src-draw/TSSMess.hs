b65f8cbfa6efa3c200ed7b0dcf2e98881adda9a5
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-08-09 15:49:38 -0400

    Load basic images

--- src-draw/TSSMess.hs
+++ src-draw/TSSMess.hs
@@ -12,4 +12,10 @@ import TSSMess.Data
 import TSSMess.Types
 
 doTssMess :: TSSMessCommands -> (Diagram B R2 -> IO ()) -> IO ()
-doTssMess = undefined
+doTssMess m rf = do
+  fImgs <- mapM loadImage
+    [ "src-draw/res/wars/sign_fed_64x64.png"
+    , "src-draw/res/wars/sign_emp_64x64.png"
+    , "src-draw/res/wars/sign_uni_64x64.png"
+    ]
+  rf undefined
